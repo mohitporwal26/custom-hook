37ffd575cc252fbcdddb3dda9a5409412b2d20da
Mohit Porwal <mohit.p@simformsolutions.com>
2022-10-18 09:52:05 +0530

    useLocalStorageHook

--- src/Component/Dashboard.jsx
+++ src/Component/Dashboard.jsx
@@ -1,19 +1,23 @@
 import { useState } from "react";
+import useLocalStorage from "../useLocalStorageHook/useLocalStorage";
 import { usePrevious } from "../usepreviousHook/usePrevious";
 
 const cities = ["Ahmedabad", "Surat", "Gandhinagar", "Delhi", "Mumbai"];
 
 export const Dashboard = () => {
   const [selection, setSelections] = useState(cities[0]);
-  const previousValue = usePrevious(selection) ?? '';
+  const previousValue = usePrevious(selection) ?? "";
+  const [theme, setTheme] = useLocalStorage("theme", "dark");
 
   return (
-    <div>
+    <div style={{ background: theme === "light" ? "white" : "grey" }}>
       <h4>Select Values</h4>
-      <div>
+      <div style={{ margin: "20px" }}>
         <select onChange={(e) => setSelections(e.target.value)}>
           {cities.map((city, i) => (
-            <option value={city} key={i}>{city}</option>
+            <option value={city} key={i}>
+              {city}
+            </option>
           ))}
         </select>
         <br />
@@ -23,6 +27,13 @@ export const Dashboard = () => {
           <span>{`Current : ${selection}`}</span>
         </div>
       </div>
+      <button
+        onClick={() =>
+          theme === "light" ? setTheme("dark") : setTheme("light")
+        }
+      >
+        Toggle theme {theme}
+      </button>
     </div>
   );
 };
