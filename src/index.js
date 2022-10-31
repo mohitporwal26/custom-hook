a5833d950f6d1d35e3f7ca5b394670f2da6efb1d
Mohit Porwal <mohit.p@simformsolutions.com>
2022-10-31 13:09:11 +0530

    NPM Changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export { default as useToggle } from "./useToggle/useToggle";
+export { default as usePrevious } from "./usePreviousHook/usePrevious";
+export { default as useDebounce } from "./useDebouncing/useDebounce";
+export { default as useLocalStorage } from "./useLocalStorageHook/useLocalStorage";
+export { default as useMousePosition } from "./useMousePosition/useMousePosition";
--- src/useMousePosition/useMousePosition.jsx
+++ src/useMousePosition/useMousePosition.jsx
@@ -1,16 +1,5 @@
 import { useState, useEffect } from 'react';
 
-/**
- * @typedef {Object} MousePosition
- * @property {number} x - X Coordinate of the mouse
- * @property {number} y - Y Coordinate of the mouse.
- */
-
-/**
- *
- * Custom hook which return an object with current coordinates of mouse pointer.
- * @return {MousePosition} Object containing the coordinates of the mouse pointer.
- */
 const useMousePosition = () => {
   const [coordinates, setCoordinates] = useState({});
 
