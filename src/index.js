c8bead93339a09b60777511a5dc83e91bceb4300
Mohit Porwal <mohit.p@simformsolutions.com>
2022-11-08 11:20:40 +0530

    Added New Hooks

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ export { default as usePrevious } from "./usePreviousHook/usePrevious";
 export { default as useDebounce } from "./useDebouncing/useDebounce";
 export { default as useLocalStorage } from "./useLocalStorageHook/useLocalStorage";
 export { default as useMousePosition } from "./useMousePosition/useMousePosition";
+export { default as useMediaQuery } from "./useMediaQuery/useMediaQuery";
+export { default as useCopyClipboard } from "./useCopyClipboard/useCopyClipboard";
